cd4bdd45d46301531e8a2cf275594a3655e48b50
rafterco <colinrafter82@gmail.com>
2022-07-05 09:07:08 +0100

    Initial commit

---
+++ src/main/java/com/rafter/GradleTutorial.java
@@ -0,0 +1,7 @@
+package com.rafter;
+
+public class GradleTutorial {
+    public static void main(String[] args) {
+        System.out.println("Gradle tw");
+    }
+}
---
+++ src/test/java/com/rafter/GradleTutorialTest.java
@@ -0,0 +1,12 @@
+package com.rafter;
+
+import org.junit.Test;
+
+public class GradleTutorialTest {
+
+    @Test
+    public void verifyNoExceptionThrown() {
+        GradleTutorial.main(new String[]{});
+    }
+
+}
