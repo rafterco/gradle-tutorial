554412776ff6aaeabdc661e14158af27dcd48097
rafterco <colinrafter82@gmail.com>
2022-07-05 14:47:10 +0100

    version stuff

--- src/main/java/com/rafter/GradleTutorial.java
+++ src/main/java/com/rafter/GradleTutorial.java
@@ -2,6 +2,6 @@ package com.rafter;
 
 public class GradleTutorial {
     public static void main(String[] args) {
-        System.out.println("Gradle tw");
+        System.out.println("Gradle twi");
     }
 }
